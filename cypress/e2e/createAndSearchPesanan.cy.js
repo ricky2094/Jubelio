b98eacd0a1202b6e7f94a8532dcaeea8f16592f6
ricky <rickyjn2094@gmail.com>
2024-08-08 15:20:25 +0700

    add create pesanan

--- cypress/e2e/createAndSearchPesanan.cy.js
+++ cypress/e2e/createAndSearchPesanan.cy.js
@@ -16,8 +16,20 @@ describe('template spec', () => {
     cy.get('.MuiGrid-container > [href="/sales/transactions"]').click()
     cy.get('[data-testid="ChevronRightIcon"]').click()
     cy.get('.MuiButton-contained > .d-flex').click()
-    
+    cy.get('input[placeholder="Pilih pelanggan"]').type('akulaku')
+    cy.wait(1000)
+    cy.get('input[placeholder="Pilih pelanggan"]').type('{downarrow}')
+    .type('{enter}')
+    cy.get('input[placeholder="Pilih lokasi"]').click().type('{downarrow}').type('{enter}')
+    cy.wait(1000)
+    cy.get('input[placeholder="Pilih lokasi"]').type('{downarrow}').type('{enter}')
+    cy.get('span.font-size-md.font-weight-bold').click()
+    cy.get('div.flex-row.d-flex').click()
+    cy.wait(1000)
+    cy.get('button[title="save"]').click()
+    cy.get('div.MuiAlert-message.css-1w0ym84').should('be.visible').should('have.text', 'Data berhasil disimpan')
   })
+
   it('Ensure user can search pesanan', ()=> {
     cy.get(':nth-child(4) > .false > .mr-2').click()
     cy.get('.MuiGrid-container > [href="/sales/transactions"]').click()
@@ -27,16 +39,19 @@ describe('template spec', () => {
     cy.get('.css-rfnosa > .MuiButton-root').click()
     cy.get('span.font-size-xl.font-weight-bold.mt-3').should('not.exist')
     cy.get('.MuiTableBody-root > .MuiTableRow-root > :nth-child(5) > span').should('contain.text', 'zaenal')
-
   })
 
-  it.only('Ensure user can edit pesanan', ()=> {
+  it.only
+  ('Ensure user can edit pesanan', ()=> {
     const baseText = 'ini edit'
     const currentTime = Date.now()
     const textForEdit = `${baseText} ${currentTime}` 
     cy.get(':nth-child(4) > .false > .mr-2').click()
     cy.get('.MuiGrid-container > [href="/sales/transactions"]').click()
     cy.get('[data-testid="ChevronRightIcon"]').click()
+    cy.get('#mui-8').type('zaenal')
+    cy.get('.css-rfnosa > .MuiButton-root').click()
+    cy.wait(1000)
     cy.get('.MuiTabs-flexContainer > :nth-child(1)').click()
     cy.get('.d-flex > .font-weight-lightbold').click()
     cy.get('.css-vb6e92 > .MuiButton-root').click()
